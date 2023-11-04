5005dc39a1701de46f7090dc5626cf52f592b428
Andrew Donnelly <thetallerbrother@outlook.com>
2023-11-04 13:10:11 -0700

    Parsing text file with FAANG questions

--- Back-end.py
+++ Back-end.py
@@ -1,1 +1,28 @@
 '''Will handle back-end for the project'''
+
+
+fileText = open("generalQuestions.txt", "r")
+
+text = fileText.readlines()
+
+
+questions = []
+
+for line in text:
+    if line[1] == ")":
+        if line[0] == "A":
+            pass
+        elif line[0] == "B":
+            pass
+        elif line[0] == "C":
+            pass
+        elif line[0] == "D":
+            pass
+        else:
+            questions.append(line)
+    else:
+        questions.append(line)
+
+
+
+
